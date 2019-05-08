99e176846c23b1e28956bec50b05500b8fe13c3d
Bruno <bruno.pejac@gmail.com>
2019-05-08 22:11:27 +0200

    remove duplicate video

--- src/data/videos.js
+++ src/data/videos.js
@@ -6,13 +6,6 @@ const videos = [
     tagline: 'Enjoy some classic Star Trek!!!1',
     poster: '//shaka-player-demo.appspot.com/assets/poster.jpg',
   },
-  {
-    manifestUri: '//storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd',
-    title: 'Star Trek: Angel One',
-    id: 'weofhoewh',
-    tagline: 'Enjoy some classic Star Trek!!!1',
-    poster: '//shaka-player-demo.appspot.com/assets/poster.jpg',
-  },
 ];
 
 export default videos;
