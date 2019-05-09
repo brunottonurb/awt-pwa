f0bfe2e5b5f7406854648fd84853452fbb94597b
Bruno <bruno.pejac@gmail.com>
2019-05-09 16:09:55 +0200

    Updates

---
+++ precache-manifest.9a3e51949f832b5c0104a9467b79e739.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88f562df8c611acb28902c4f4b698241",
+    "url": "/awt-pwa/index.html"
+  },
+  {
+    "revision": "0686232344cee0fdccfc",
+    "url": "/awt-pwa/static/css/main.77d2a321.chunk.css"
+  },
+  {
+    "revision": "4044fa8081214ab68d07",
+    "url": "/awt-pwa/static/js/2.24c32778.chunk.js"
+  },
+  {
+    "revision": "0686232344cee0fdccfc",
+    "url": "/awt-pwa/static/js/main.d48db164.chunk.js"
+  },
+  {
+    "revision": "d3e6c92ec33f19987f67",
+    "url": "/awt-pwa/static/js/runtime~main.a5205106.js"
+  }
+]);
